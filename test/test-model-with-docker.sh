701d5faa4c648573aab5dfd4adab728d2b2870d2
Abe <abe.chen723@gmail.com>
2020-03-06 09:04:39 +0800

    [feature] modify test script

--- test/test-model-with-docker.sh
+++ test/test-model-with-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export APP_HOME="$(cd "`dirname "$0"`"/..; pwd)"
-export IMAGE_NAME=cathay/sklearn-iris
-export IMAGE_VERSION=0.1
+export IMAGE_NAME=abechen/serving-iris-demo
+export IMAGE_VERSION=latest
 
 # run wrapper docker image
 docker run --name "iris_predictor" --rm -p 5001:5000 ${IMAGE_NAME}:${IMAGE_VERSION}
@@ -9,21 +9,19 @@ docker run --name "iris_predictor" --rm -p 5001:5000 ${IMAGE_NAME}:${IMAGE_VERSI
 # test wuth docker
 curl http://0.0.0.0:5001/predict \
     --data 'json={"data":{"ndarray":[[1.0,2.0, 3.0, 4.0]]}}' 
-: '{
-    "data":{
-        "names":[
-        "iris-setosa",
-        "iris-vericolor",
-        "iris-virginica"
-        ],
-    "ndarray":[
-        [
-            0.0364996395323677,
-            0.00964742714103532,
-            0.953852933326597
-        ]
-    ]
+: '
+  {
+    "meta": {
+      "puid": "gqrqnnn88v0o8mtbguflptrtgg",
+      "tags": {},
+    "routing": {},
+    "requestPath": {
+      "serving-iris-classifier": "abechen/serving-iris-demo:latest"
     },
-    "meta":{
-    }
-}'
+    "metrics": []
+  },
+  "data": {
+    "names": ["iris-setosa", "iris-vericolor", "iris-virginica"],
+    "ndarray": [[0.0011089286758401262, 0.012902008299819315, 0.9859890630243405]]
+  }
+'
